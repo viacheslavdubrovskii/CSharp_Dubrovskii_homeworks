68ae754356cacde0b5e9c7f89a2c4b95532d38a3
Lrunner <viacheslav.dubrovskii.@mail.ru>
2020-10-31 13:30:44 +0300

    Lesson_2 added

---
+++ CSharp_Lesson2_homework/Lesson2_ex7/HomeWork7.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_ex7
+{
+    class HomeWork7
+    {
+        // Дубровский Вячеслав
+        // 7. a) Разработать рекурсивный метод, который выводит на экран числа от a до b(a<b)
+        static void Loop(int a, int b)
+        {
+            Console.Write("{0,3} ", a);
+            if (a < b) Loop(a + 1, b);
+        }
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Введите число а");
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Введите целое число а");
+            }
+            int b;
+            Console.WriteLine("Введите число b, которое больше а");
+            while (!int.TryParse(Console.ReadLine(), out b))
+            {
+                Console.WriteLine("Введите целое число b, которое больше а");
+            }
+            if (a < b)
+            {
+                Loop(a, b);
+            }
+            else
+            {
+                Console.WriteLine("Число b должно было быть больше а. Не буду ничего выводить.");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
